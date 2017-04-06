2383750a7fe3c8d643d1e0081bada6c3a0158ef3
Julio Tentor <jtentor@gmail.com>
2017-04-06 09:00:19 -0300

    Test some samples

--- app/src/main/cpp/demo.cpp
+++ app/src/main/cpp/demo.cpp
@@ -9,6 +9,7 @@
  */
 
 #include <string>
+#include <sstream>
 
 #include "json.hpp"
 
@@ -20,11 +21,51 @@ using json = nlohmann::json;
  * @return string with json data
  */
 std::string jsonDemo() {
+    std::ostringstream out;
+
     int var1 = 78;
     bool var2 = true;
 
     json j1 = {"algun texto", "otro texto", 15, 22, var1, var2};
+
+    out << "j1 dump      " << j1.dump() << std::endl;
+    out << "j1 to string " << j1 << std::endl;
+
     json j2 = json::array({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
 
-    return j1.dump() + "\n" + j2.dump();
+    out << "j2 dump      " << j2.dump() << std::endl;
+    out << "j2 to string " << j2 << std::endl;
+
+    j2.insert(j2.end(), {101, 102, 103});
+    j2.insert(j2.begin(), {-1, -2, -3});
+
+    out << "after inserts" << std::endl;
+    out << "j2 dump      " << j2.dump() << std::endl;
+    out << "j2 to string " << j2 << std::endl;
+
+    json j3 = json::object({{"text1", "algun texto"},
+                            {"text2", "otro texto"},
+                            {"num1",  15},
+                            {"num2",  22},
+                            {"var1",  var1},
+                            {"var2",  var2}});
+
+    out << "j3 dump      " << j3.dump() << std::endl;
+    out << "j3 to string " << j3 << std::endl;
+
+/*
+ * From Notes at https://github.com/Newbrick/json#notes
+ *
+ * By default, the library does not preserve the insertion order of object elements. This is
+ * standards-compliant, as the JSON standard defines objects as "an unordered collection of
+ * zero or more name/value pairs". If you do want to preserve the insertion order, you can
+ * specialize the object type with containers like tsl::ordered_map or nlohmann::fifo_map.
+ */
+    j3.push_back({"push_back", true});
+
+    out << "after push_back" << std::endl;
+    out << "j3 dump      " << j3.dump() << std::endl;
+    out << "j3 to string " << j3 << std::endl;
+
+    return out.str();
 }
